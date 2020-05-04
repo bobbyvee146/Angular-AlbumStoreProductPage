dbd0fb03c26d9c6aa43ddd2d6bb25c470a5ea01d
robbyGregg <robbygregg25@gmail.com>
2020-05-04 13:00:22 -0400

    part5

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,11 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs/Observable';
 import { Album } from './album';
-import { Track } from './track';
-
-// add this so that it will pass the test - but this is not used
 import 'rxjs/add/operator/map'
 
 
